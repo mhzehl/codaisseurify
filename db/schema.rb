5a994b06ea06dfc351095ad08fbcefd7c172c01e
Melvin Zehl <mhzehl@gmail.com>
2017-01-15 22:06:15 +0100

    Update seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170115205121) do
+ActiveRecord::Schema.define(version: 20170115205340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,9 @@ ActiveRecord::Schema.define(version: 20170115205121) do
     t.string   "video_url"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "artist_id"
+    t.index ["artist_id"], name: "index_songs_on_artist_id", using: :btree
   end
 
+  add_foreign_key "songs", "artists"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,17 +2,17 @@ Song.delete_all
 Artist.delete_all
 
 # Songs -- A song belongs to an artist -- children
-song1 = Song.create!( { name: “Uprising”, release_year: 2009, video_url: “https://www.youtube.com/watch?v=w8KQmps-Sog” } )
-song2 = Song.create!( { name: “Madness”, release_year: 2012, video_url: ”https://www.youtube.com/watch?v=Ek0SgwWmF9w” } )
-song3 = Song.create!( { name: “Creep”, release_year: 1993, video_url: ”https://www.youtube.com/watch?v=XFkzRNyygfk” } )
-song4 = Song.create!( { name: “Karma Police”, release_year: 1997, video_url: ”https://www.youtube.com/watch?v=IBH97ma9YiI” } )
-song5 = Song.create!( { name: “Sharp Dressed Man”, release_year: 1983, video_url: ”https://www.youtube.com/watch?v=7wRHBLwpASw” } )
-song6 = Song.create!( { name: “La Grange”, release_year: 1973, video_url: ”https://www.youtube.com/watch?v=aB4nH8qx2IM” } )
+song1 = Song.create( { name: "Uprising", release_year: 2009, video_url: "https://www.youtube.com/watch?v=w8KQmps-Sog" } )
+song2 = Song.create( { name: "Madness", release_year: 2012, video_url: "https://www.youtube.com/watch?v=Ek0SgwWmF9w" } )
+song3 = Song.create( { name: "Creep", release_year: 1993, video_url: "https://www.youtube.com/watch?v=XFkzRNyygfk" } )
+song4 = Song.create( { name: "Karma Police", release_year: 1997, video_url: "https://www.youtube.com/watch?v=IBH97ma9YiI" } )
+song5 = Song.create( { name: "Sharp Dressed Man", release_year: 1983, video_url: "https://www.youtube.com/watch?v=7wRHBLwpASw" } )
+song6 = Song.create( { name: "La Grange", release_year: 1973, video_url: "https://www.youtube.com/watch?v=aB4nH8qx2IM" } )
 
 # Artists -- An artist has many songs -- parent
-artist1 = Artist.create!( { name: “Muse”, genre: “Progressive Rock”, band_members: 3, image_url: “http://bit.ly/2iv798V” } )
-artist2 = Artist.create!( { name: “Radiohead”, genre: “Alternative Rock”, band_members: 5, image_url: “http://bit.ly/2irgy64” } )
-artist3 = Artist.create!( { name: “ZZ Top”, genre: “Blues Rock”, band_members: 6, image_url: “http://bit.ly/2jyL8Yv” } )
+artist1 = Artist.create( { name: "Muse", genre: "Progressive Rock", band_members: 3, image_url: "http://bit.ly/2iv798V" } )
+artist2 = Artist.create( { name: "Radiohead", genre: "Alternative Rock", band_members: 5, image_url: "http://bit.ly/2irgy64" } )
+artist3 = Artist.create( { name: "ZZ Top", genre: "Blues Rock", band_members: 6, image_url: "http://bit.ly/2jyL8Yv" } )
 
 artist1.songs << [song1, song2]
 artist1.save
