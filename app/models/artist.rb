84798983583a31d0e1b194de0041209198b2b2c3
Melvin Zehl <mhzehl@gmail.com>
2017-01-23 00:58:10 +0100

    Add validations to Artist

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,8 @@
 class Artist < ApplicationRecord
   has_many :songs, dependent: :destroy
+
+  validates :name, presence: true
+  validates :genre, presence: true, length: {maximum: 25}
+  validates :band_members, presence: true
+  validates :image_url, presence: true
 end
