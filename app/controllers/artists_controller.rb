514650b69dc1f75e231a36cbf091cdd044d660d9
Melvin Zehl <mhzehl@gmail.com>
2017-01-22 21:19:57 +0100

    Add helper method for sorting

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,11 +1,20 @@
 class ArtistsController < ApplicationController
+  helper_method :sort_column, :sort_direction
 
   def index
-    @artists = Artist.order(params[:sort] + " " + params[:direction])
+    @artists = Artist.order(sort_column + " " + sort_direction)
   end
 
   def show
     @artist = Artist.find(params[:id])
   end
 
+  private
+  def sort_column
+    Artist.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
 end
