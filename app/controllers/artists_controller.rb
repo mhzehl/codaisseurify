a05daff6e5a1e7ead1843c54c2f831693d13ecbf
Melvin Zehl <mhzehl@gmail.com>
2017-01-22 15:31:32 +0100

    Add direction to sorting

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,7 @@
 class ArtistsController < ApplicationController
 
   def index
-    @artists = Artist.order(params[:sort])
+    @artists = Artist.order(params[:sort] + " " + params[:direction])
   end
 
   def show
