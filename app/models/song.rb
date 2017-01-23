35d04e8ec2698494d65dd2c0e4870f988d389145
Melvin Zehl <mhzehl@gmail.com>
2017-01-23 01:00:12 +0100

    Add validations Song

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,7 @@
 class Song < ApplicationRecord
   belongs_to :artist
+
+  validates :name, presence: true
+  validates :release_year: true, length: {maximum: 4}
+  validates :video_url, presence: true
 end
